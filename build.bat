cdfee65adbf70ec81d347c5ad529f8e09548c472
Khanh N. Dang <dnk0904@gmail.com>
2015-07-12 09:41:28 +0900

    new page

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 chcp 65001
-jekyll build --source C:\Users\Khanh\Documents\GitHub\grayscale-theme-master --destination C:\Users\Khanh\Documents\GitHub\khanhdang.github.io
+jekyll build --source C:\Users\Khanh\Documents\GitHub\long-haul-theme --destination C:\Users\Khanh\Documents\GitHub\khanhdang.github.io
 
