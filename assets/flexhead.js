7c7ee19e0e0e46a7ebc0f4ea178d51dd03f52fbe
Khanh N. Dang <khanhdang@users.noreply.github.com>
2021-07-09 17:32:41 +0700

    add flexheader js

---
+++ assets/flexhead.js
@@ -0,0 +1,14 @@
+window.onscroll = function () {
+    var scroll = window.pageYOffset;
+    var navbar = document.getElementById("nav-container");
+
+    if (scroll >= 100) {
+        navbar.classList.add("small-header");
+        navbar.classList.remove("big-header");
+    }
+    else {
+        navbar.classList.remove("small-header");
+        navbar.classList.add("big-header");
+
+    }
+};
