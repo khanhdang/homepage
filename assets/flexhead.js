14f565852cfa64dfef83dd680f211d85f5c5d8ca
Khanh N. Dang <dnk0904@gmail.com>
2021-07-10 20:13:36 +0700

    fix font, fix animation break

--- assets/flexhead.js
+++ assets/flexhead.js
@@ -2,12 +2,12 @@ window.onscroll = function () {
     var scroll = window.pageYOffset;
     var navbar = document.getElementById("nav-container");
 
-    if (scroll >= 100) {
+    if (scroll >= 150) {
         navbar.classList.add("small-header");
         navbar.classList.remove("big-header");
     }
     else {
-        if (scroll < 80) {
+        if (scroll < 20) {
             navbar.classList.remove("small-header");
             navbar.classList.add("big-header");
         }
