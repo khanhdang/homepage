2aeef91fac78c8f672fc1754743e5dd8ee3b13a7
Khanh N. Dang <dnk0904@gmail.com>
2021-08-13 17:27:16 +0700

    add flexible logo

--- assets/flexhead.js
+++ assets/flexhead.js
@@ -1,15 +1,24 @@
 window.onscroll = function () {
     var scroll = window.pageYOffset;
     var navbar = document.getElementById("nav-container");
-
+    var logo = document.getElementById("logo");
+    if (document.documentElement.clientWidth <= 992 ) {
+        logo.src = "/assets/logo_long.svg"
+    }
     if (scroll >= 150) {
         navbar.classList.add("small-header");
         navbar.classList.remove("big-header");
+        logo.src = "/assets/logo_long.svg"
+
     }
     else {
         if (scroll < 20) {
             navbar.classList.remove("small-header");
             navbar.classList.add("big-header");
+            if (document.documentElement.clientWidth > 992 ) {
+                logo.src = "/assets/logo.svg"
+            }
+            
         }
 
     }
